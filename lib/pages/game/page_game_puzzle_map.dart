8296c3fa4b2c9a981b7c0894a78f55c02a44bd4c
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2025-12-17 17:02:35 +0800

    第六個遊戲

--- lib/pages/game/page_game_puzzle_map.dart
+++ lib/pages/game/page_game_puzzle_map.dart
@@ -8,7 +8,7 @@ import 'package:life_pilot/controllers/game/controller_game_puzzle_map.dart';
 import 'package:life_pilot/core/const.dart';
 import 'package:life_pilot/models/game/model_game_puzzle_map.dart';
 import 'package:life_pilot/pages/game/page_game_grammar.dart';
-import 'package:life_pilot/pages/game/page_game_sentence_say.dart';
+import 'package:life_pilot/pages/game/page_game_say_sentence.dart';
 import 'package:life_pilot/services/game/service_game.dart';
 import 'package:provider/provider.dart';
 
