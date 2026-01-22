2132c1400cb705e06e7dbf9c0470729feb709e55
CT01795 <CT01795@users.noreply.github.com>
2026-01-22 06:55:15 +0000

    deploy: 4d1339fdba89815627417b6fd5367c7e0af4df47

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2881295827"
+    serviceWorkerVersion: "2445437793"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -113,7 +113,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "ec7e6ddece6ee23915008c836ab1b961",
+"flutter_bootstrap.js": "68f380b97f0a085bd8d364e995b0ac7f",
 "version.json": "3d24f8e1c43390d4ffe74cc3fce20e2a",
 "main.dart.js": "f11111faf0840152aa57d1c2dc9f1aa4"};
 // The application shell files that are downloaded before a service worker can
