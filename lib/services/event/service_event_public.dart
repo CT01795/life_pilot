12507c498421a517cf7208b1119cd5dd3dbdab0a
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-02-02 12:49:49 +0800

    修復遊戲

--- lib/services/event/service_event_public.dart
+++ lib/services/event/service_event_public.dart
@@ -47,26 +47,39 @@ class ServiceEventPublic {
   Future<void> fetchAndSaveAllEvents() async {
     //==================================== 取得目前資料庫事件 ====================================
     Set<String> dbNameSet = (await ServiceEvent().getEvents(
-      tableName: TableNames.recommendedEvents,
-      inputUser: AuthConstants.sysAdminEmail,
-    ) ?? []).map((e) => e.name).where((name) => name.isNotEmpty).toSet();
+              tableName: TableNames.recommendedEvents,
+              inputUser: AuthConstants.sysAdminEmail,
+            ) ??
+            [])
+        .map((e) => e.name)
+        .where((name) => name.isNotEmpty)
+        .toSet();
 
     DateTime today = DateTime.now();
     today = DateTime(today.year, today.month, today.day);
     //==================================== 取得外部資源事件 strolltimesUrl ====================================
-    final strolltimesUrl = 'https://strolltimes.com/weekend-events/';
+    final strolltimesUrl =
+        "https://news.strolltimes.com/events/weekend/"; //'https://strolltimes.com/weekend-events/';
     if (await checkEventsUrl(strolltimesUrl, today)) {
       try {
         List<EventItem> strolltimesList =
             await fetchPageEventsStrolltimes(strolltimesUrl, today) ?? [];
 
         //==================================== strolltimesList事件寫入 ====================================
-        List<Map> dataList = strolltimesList.isEmpty ? [] : (dbNameSet.isEmpty ? strolltimesList : strolltimesList.where((e) {
-          final name = e.name;
-          return !dbNameSet.contains(name);
-        }).toList()).map((e) => e.toJson()).toList();
+        List<Map> dataList = strolltimesList.isEmpty
+            ? []
+            : (dbNameSet.isEmpty
+                    ? strolltimesList
+                    : strolltimesList.where((e) {
+                        final name = e.name;
+                        return !dbNameSet.contains(name);
+                      }).toList())
+                .map((e) => e.toJson())
+                .toList();
 
-        if(dataList.isNotEmpty) await client.from(TableNames.recommendedEvents).insert(dataList);
+        if (dataList.isNotEmpty) {
+          await client.from(TableNames.recommendedEvents).insert(dataList);
+        }
       } on Exception catch (ex) {
         logger.e(ex);
       }
@@ -92,7 +105,6 @@ class ServiceEventPublic {
   }
 
   //==================================== 取得外部資源事件 Accupass ====================================
- 
 
   //==================================== 取得外部資源事件 strolltimesUrl ====================================
   Future<List<EventItem>?> fetchPageEventsStrolltimes(
@@ -101,91 +113,73 @@ class ServiceEventPublic {
       'User-Agent': 'Mozilla/5.0 (compatible; StrollTimesCrawler/1.0)'
     });
     if (res.statusCode != 200) return [];
-
     final document = parse(res.body);
-    // 抓所有 <a>，找 href 以 /weekend-events/ 開頭
-    // 1️⃣ 找第一筆 /weekend-events/ 連結
-    final eventLinkElement = document
-        .querySelectorAll('a')
+
+    // 取側邊欄最新的活動子頁
+    final links = document
+        .querySelectorAll('ul.menu__list li a')
         .where((a) =>
-            a.attributes['href']
-                ?.startsWith('https://strolltimes.com/weekend-events/') ??
-            false)
+            a.attributes['href']?.startsWith('/events/weekend/') ?? false)
         .cast<Element>()
         .toList();
 
-    final firstEvent =
-        eventLinkElement.isNotEmpty ? eventLinkElement.first : null;
+    if (links.isEmpty) return [];
 
-    String? firstEventLink;
-    if (firstEvent != null) {
-      firstEventLink = firstEvent.attributes['href']!;
-      final firstEventTitle = firstEvent.attributes['title'];
-      logger.i('第一筆活動連結: $firstEventLink, $firstEventTitle');
-    } else {
-      return null;
-    }
-    // 2️⃣ 點進去抓活動頁面內容
-    final eventResp = await http.get(Uri.parse(firstEventLink));
-
-    if (eventResp.statusCode != 200) {
-      logger.e('抓活動頁面失敗: ${eventResp.statusCode}');
-      return null;
-    }
-
-    final eventDoc = parse(eventResp.body);
-
-    // 找到所有 <h2> 標題
-    final h2List = eventDoc.querySelectorAll('h2.wp-block-heading');
+    Set<String> tmpSet = {};
     List<EventItem> tmpList = [];
-    final Uuid uuid = const Uuid();
-    for (var h2 in h2List) {
-      final title = h2.text.trim().replaceAll("活動", constEmpty);
-      logger.i('=== 標題: $title ===');
-      if (title.contains("地圖")) {
-        continue;
-      }
-      // 找到該 h2 之後的 <figure class="wp-block-table">
-      var sibling = h2.nextElementSibling;
-      while (sibling != null) {
-        if (sibling.localName == 'figure' &&
-            sibling.classes.contains('wp-block-table')) {
-          final table = sibling.querySelector('table');
-          if (table != null) {
-            final rows = table.querySelectorAll('tbody tr');
-            for (var row in rows) {
-              final cells = row.querySelectorAll('td');
-              if (cells.length >= 4) {
-                DateTime? startDate = DateTime.tryParse(cells[0].text.trim());
-                DateTime? endDate = DateTime.tryParse(cells[1].text.trim());
-                if (startDate != null &&
-                    endDate != null &&
-                    !endDate.isBefore(today)) {
-                  final location = cells[3].text.trim();
-                  // 取得活動名稱和 href
-                  final aTag = cells[2].querySelector('a');
-                  final eventName = aTag?.text.trim() ?? '';
-                  final eventHref = aTag?.attributes['href'] ?? '';
-                  final eventItem = EventItem(
-                    id: uuid.v4(),
-                    masterUrl: eventHref,
-                    startDate: startDate,
-                    endDate: endDate,
-                    city: title,
-                    location: location,
-                    name: eventName,
-                    account: AuthConstants.sysAdminEmail,
-                  );
-                  tmpList.add(eventItem);
-                }
+    final uuid = const Uuid();
+    for (int i = 1; i < links.length; i++) {
+      final latestPath = links[i].attributes['href'];
+      if (latestPath == null) continue;
+
+      String url2 = 'https://news.strolltimes.com$latestPath';
+      final res2 = await http.get(Uri.parse(url2), headers: {
+        'User-Agent': 'Mozilla/5.0 (compatible; StrollTimesCrawler/1.0)'
+      });
+      if (res2.statusCode != 200) return [];
+      final document2 = parse(res2.body);
+
+      // 找到所有 <h2> 標題
+      final h2List = document2.querySelectorAll('h2');
+      for (var h2 in h2List) {
+        String title = h2.text.trim();
+        if (!title.contains("活動")) continue;
+        title = title.replaceAll("活動", '');
+
+        // 找該 h2 之後的 <figure class="wp-block-table">
+        var sibling = h2.nextElementSibling;
+        if (sibling == null) continue;
+        final rows = sibling.querySelectorAll('tbody tr');
+        for (var row in rows) {
+          final cells = row.querySelectorAll('td');
+          if (cells.length >= 4) {
+            DateTime? startDate =
+                DateTime.tryParse(cells[0].text.trim().replaceAll('/', '-'));
+            DateTime? endDate =
+                DateTime.tryParse(cells[1].text.trim().replaceAll('/', '-'));
+            if (startDate != null &&
+                endDate != null &&
+                !endDate.isBefore(today)) {
+              final location = cells[3].text.trim();
+              final aTag = cells[2].querySelector('a');
+              final eventName = aTag?.text.trim() ?? '';
+              final eventHref = aTag?.attributes['href'] ?? '';
+              if (!tmpSet.contains(eventName)) {
+                tmpList.add(EventItem(
+                  id: uuid.v4(),
+                  masterUrl: eventHref,
+                  startDate: startDate,
+                  endDate: endDate,
+                  city: title,
+                  location: location,
+                  name: eventName,
+                  account: AuthConstants.sysAdminEmail,
+                ));
+                tmpSet.add(eventName);
               }
             }
           }
-          break; // 找到 table 就跳出 while
-        } else if (sibling.innerHtml.contains("未蒐集")) {
-          break;
         }
-        sibling = sibling.nextElementSibling;
       }
     }
     return tmpList;
