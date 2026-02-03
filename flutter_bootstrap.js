04fc1bbb36736e539e1cc824e4914ce9ee3bef61
CT01795 <CT01795@users.noreply.github.com>
2026-02-03 03:13:34 +0000

    deploy: b8e4f09127c10b2b1cb787aaccf81fa30062a51b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2486597491"
+    serviceWorkerVersion: "678965232"
   }
 });
