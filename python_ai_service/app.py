dee757fc47ca1490e1881231ae0346b80d5b717d
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-03-24 15:11:40 +0800

    Add python ai service

--- python_ai_service/app.py
+++ python_ai_service/app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 import pandas as pd
 from sqlalchemy import create_engine
@@ -5,8 +6,7 @@ from sqlalchemy import create_engine
 app = FastAPI()
 
 # Supabase 資料庫連線字串
-DB_URL = "postgresql://postgres.ccktdpycnferbrjrdtkp:QN4uJPxHzWR64e2u@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
-
+DB_URL = os.getenv("DB_URL")  # 從render環境變數取得
 engine = create_engine(DB_URL)
 
 @app.get("/")
