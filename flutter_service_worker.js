43967eb4edc8d52c2bf1bd997ecdf909728807c4
CT01795 <CT01795@users.noreply.github.com>
2026-04-20 03:10:50 +0000

    deploy: d8500c6a0ff2f4ba95e4dbeff4250b8736ec1104

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -115,7 +115,7 @@ const RESOURCES = {"assets/assets/maps/oceania.png": "568ec09c665f833638f14a245e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a20cbb21e0ad3ecc7e2468b87e17d222",
+"flutter_bootstrap.js": "ed469ec0710dcb981572160d4f7c21ed",
 "main.dart.js": "ab96312e306865d8fc0d69eb79ae8a32",
 "version.json": "3d24f8e1c43390d4ffe74cc3fce20e2a",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
