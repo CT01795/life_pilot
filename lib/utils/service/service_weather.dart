382f83f30921e978c390b17a9ef13fc3772570e3
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-04-24 09:39:25 +0800

    減少IO量

--- lib/utils/service/service_weather.dart
+++ lib/utils/service/service_weather.dart
@@ -164,12 +164,18 @@ class ServiceWeather {
     final DateTime yesterday = today.add(Duration(days: -1));
     final DateTime thisWeek = today.add(Duration(days: 8));
     for (final e in events) {
-      if (!((e.endDate == null && thisWeek.compareTo(e.startDate!) == 1 &&
-          yesterday.compareTo(e.startDate!) == -1) || (e.endDate != null && thisWeek.compareTo(e.startDate!) == 1 &&
-          yesterday.compareTo(e.endDate!) == -1))) {
-        continue;
+      if (e.endDate == null) {
+        if (!(thisWeek.compareTo(e.startDate!) == 1 &&
+            yesterday.compareTo(e.startDate!) == -1)) {
+          continue;
+        }
+      } else {
+        if (!(thisWeek.compareTo(e.startDate!) == 1 &&
+            yesterday.compareTo(e.endDate!) == -1)) {
+          continue;
+        }
       } //當只有start date, 日期必須是今日或一周內才要看天氣
-       //strat date 必須在一周內開始, 且結束日必須至少今天開始才要看天氣
+      //strat date 必須在一周內開始, 且結束日必須至少今天開始才要看天氣
       if (!e.hasLocation) continue;
       if (WeatherCacheStore.I.cache.containsKey(e.locationDisplay)) continue;
 
