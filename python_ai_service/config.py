e4aa5e6751bec758e90770714a43fc79a6265cd4
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-06-09 11:25:21 +0800

    add keep alive workflow

--- python_ai_service/config.py
+++ python_ai_service/config.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DB_URL = os.getenv("DB_URL")
+#DB_URL = os.getenv("DB_URL")
+DB_URL = "postgresql://postgres.ccktdpycnferbrjrdtkp:QN4uJPxHzWR64e2u@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres" #os.getenv("DB_URL")  # 從render環境變數取得
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine) 
