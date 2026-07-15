56ee6984c854a52e98636afdd29f7c846f61405f
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-07-15 08:21:51 +0800

    新增韓翻中的題目

--- lib/utils/tts/tts_web.dart
+++ lib/utils/tts/tts_web.dart
@@ -15,11 +15,16 @@ Future<void> speakWeb(String text, {String? group, bool? isQuestion}) async {
   if ((group.contains("中翻英") && isQuestion) ||
       (group.contains("英翻中") && !isQuestion) ||
       (group.contains("日翻中") && !isQuestion) ||
-      (group.contains("中翻日") && isQuestion)) {
+      (group.contains("中翻日") && isQuestion) ||
+      (group.contains("韓翻中") && !isQuestion) ||
+      (group.contains("中翻韓") && isQuestion)) {
     utterance.lang = 'zh-TW';
   } else if ((group.contains("中翻日") && !isQuestion) ||
       (group.contains("日翻中") && isQuestion)) {
     utterance.lang = 'ja-JP';
+  } else if ((group.contains("中翻韓") && !isQuestion) ||
+      (group.contains("韓翻中") && isQuestion)) {
+    utterance.lang = 'ko-KR';
   } else {
     utterance.lang = 'en-US';
   }
