4f8c0a3dbf116a53194fd4999fcb2d4c0697aa73
CT01795 <CT01795@users.noreply.github.com>
2026-08-12 05:47:16 +0000

    deploy: f6d32425bfa1defdfb9b8ae014678c36d427c931

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "e976f616365bfaf21427bd2cefdabefc",
+"flutter_bootstrap.js": "f2cb46bff0ed5cda2ce4f6934cd8c36a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
