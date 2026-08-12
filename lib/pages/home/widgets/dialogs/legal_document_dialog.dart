e5ccd646eb05207688f9fa0017f879dd4f851518
Shinhui Shih(示欣惠) <shinhui.shih@c-techone.com>
2026-08-12 15:44:11 +0800

    feat: show legal documents in app

---
+++ lib/pages/home/widgets/dialogs/legal_document_dialog.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:life_pilot/l10n/app_localizations.dart';
+
+class LegalDocumentDialog extends StatelessWidget {
+  const LegalDocumentDialog({
+    super.key,
+    required this.title,
+    required this.assetPath,
+  });
+
+  final String title;
+  final String assetPath;
+
+  @override
+  Widget build(BuildContext context) {
+    final loc = AppLocalizations.of(context)!;
+
+    return AlertDialog(
+      titlePadding: const EdgeInsets.fromLTRB(24, 16, 8, 0),
+      title: Row(
+        children: [
+          Expanded(child: Text(title)),
+          IconButton(
+            icon: const Icon(Icons.close),
+            tooltip: loc.close,
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+      content: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 720, maxHeight: 520),
+        child: FutureBuilder<String>(
+          future: rootBundle.loadString(assetPath),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(child: Text('Unable to load this document.'));
+            }
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            return SingleChildScrollView(child: HtmlWidget(snapshot.data!));
+          },
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text(loc.close),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/home/widgets/user_menu_button.dart
+++ lib/pages/home/widgets/user_menu_button.dart
@@ -2,25 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:life_pilot/auth/controller_auth.dart';
 import 'package:life_pilot/auth/model_auth_view.dart';
 import 'package:life_pilot/pages/home/widgets/dialogs/draggable_resizable_dialog.dart';
+import 'package:life_pilot/pages/home/widgets/dialogs/legal_document_dialog.dart';
 import 'package:life_pilot/feedback/controller_feedback.dart';
 import 'package:life_pilot/feedback/page_feedback.dart';
 import 'package:life_pilot/feedback/service_feedback.dart';
 import 'package:life_pilot/l10n/app_localizations.dart';
 import 'package:life_pilot/utils/const.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class UserMenuButton extends StatelessWidget {
   const UserMenuButton({
     super.key,
   });
-
-  static final Uri _privacyPolicyUrl = Uri.parse(
-    'https://ct01795.github.io/life_pilot/privacy.html',
-  );
-  static final Uri _termsOfServiceUrl = Uri.parse(
-    'https://ct01795.github.io/life_pilot/terms.html',
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -43,10 +36,18 @@ class UserMenuButton extends StatelessWidget {
             _openFeedback(context);
             break;
           case "privacyPolicy":
-            _openLink(context, _privacyPolicyUrl);
+            _openLegalDocument(
+              context,
+              title: loc.privacyPolicy,
+              assetPath: 'web/privacy.html',
+            );
             break;
           case "termsOfService":
-            _openLink(context, _termsOfServiceUrl);
+            _openLegalDocument(
+              context,
+              title: loc.termsOfService,
+              assetPath: 'web/terms.html',
+            );
             break;
           case "logout":
             auth.logout.call();
@@ -139,13 +140,14 @@ class UserMenuButton extends StatelessWidget {
     );
   }
 
-  Future<void> _openLink(BuildContext context, Uri url) async {
-    if (await launchUrl(url, mode: LaunchMode.externalApplication)) return;
-    if (!context.mounted) return;
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-          content: Text('Unable to open the link. Please try again.')),
+  void _openLegalDocument(
+    BuildContext context, {
+    required String title,
+    required String assetPath,
+  }) {
+    showDialog(
+      context: context,
+      builder: (_) => LegalDocumentDialog(title: title, assetPath: assetPath),
     );
   }
 
