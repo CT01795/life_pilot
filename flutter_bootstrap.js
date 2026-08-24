15df68a19fc5229c98225e7c81303427a7a21d05
CT01795 <CT01795@users.noreply.github.com>
2026-08-24 07:19:59 +0000

    deploy: 6f5e165df7aa7d9e14f9582c80d92c586f6934fe

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3478472892"
+    serviceWorkerVersion: "2962329391"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -123,8 +123,8 @@ const RESOURCES = {"version.json": "3d24f8e1c43390d4ffe74cc3fce20e2a",
 "assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.html": "16911fcc170c8af1c5457940bd0bf055",
 "assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.css": "5a8d0222407e388155d7d1395a75d5b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"main.dart.js": "bbe15c61de8744784d5ec44a44ecf2d2",
-"flutter_bootstrap.js": "9b3b64eac0bdb3706964f7ca86b62771",
+"main.dart.js": "9e98d20554795b2471790e8aa736414f",
+"flutter_bootstrap.js": "7299ce2fc303eb61fca6a739702f9c39",
 "manifest.json": "7419a1d99de3454464351381ebc05cc3"};
 // The application shell files that are downloaded before a service worker can
 // start.
