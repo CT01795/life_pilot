b76846488d9522e5ef15f06765bae8b552e20073
CT01795 <CT01795@users.noreply.github.com>
2026-09-09 06:01:55 +0000

    deploy: fb4ada8f2845cc8e5e98b7470cfcc37a35555509

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "7419a1d99de3454464351381ebc05cc3",
 "version.json": "3d24f8e1c43390d4ffe74cc3fce20e2a",
-"main.dart.js": "0c818ec448907e4977cdea70e6489549",
-"flutter_bootstrap.js": "90d1833d1305650a3de0fae5c0616375",
+"main.dart.js": "0d5e5ccf3d8daa71a62d5782d80ecebc",
+"flutter_bootstrap.js": "5bf4931359c9034f6708de6e8d67216e",
 "privacy.html": "c4d8d77a261489954fabaf2322f36ac4",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
